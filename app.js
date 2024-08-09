c4dc012960291a40faac804b85cafc3c15f530ad
AzatSardaryan <azat.sardaryan1@hotmail.com>
2024-08-09 13:35:53 +0100

    Added handling of the images once we recieve the data back from the API.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const submitIcon = document.querySelector("#submit-icon")
 const inputElement = document.querySelector("input")
-
+const imageSection = document.querySelector(".image-section")
 const getImages = async () => {
 
     const options = {
@@ -10,17 +10,26 @@ const getImages = async () => {
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            "prompt": inputElement.value, 
-            "n": 2, 
-            "size": "1024x1024"
+            prompt : inputElement.value, 
+            n : 2, 
+            size : "1024x1024"
         })
     }
-
+    
     try 
     {
         const response = await fetch('https://api.openai.com/v1/images/generations', options)
         const data = await response.json()
         console.log(data)
+        data?.data.forEach(imageObject => {
+            const imageContainier = document.createElement('div')
+            imageContainier.classList.add('image-contanier')
+            const imageElement = document.createElement('image')
+            imageElement.setAttribute('src', imageObject.url)
+            imageContainier.append(imageElement)
+            imageSection.append(imageContainier)
+        })
+
     } 
     catch(err) 
     {
